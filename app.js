8935b15c13bb6be2fcbf3c62e97b6bffa13b41d5
Renato Francia <rmfranciacastillo@gmail.com>
2019-03-14 21:25:15 -0500

    Added: Chalk and logger

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+// Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Logger Middleware
+app.use(logger('dev'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start Express Server
 app.listen(PORT, () => {
-  console.log(`Running server on port: ${PORT}`);
+  console.log(`%s Running server on port: ${PORT}`, chalk.green('✓'));
+  console.log('  Press CTRL-C to stop\n');
 });
