c46dc7bbd24586ae09d9df5124a9248c3225e57a
Renato Francia <rmfranciacastillo@gmail.com>
2019-03-14 20:59:11 -0500

    Added: Eslint, husky

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Running server on port: ${PORT}`);
+});
